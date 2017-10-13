964de2829bc05151f283f2879a234c2c6f8a3468
Clark Murray <clarkamurray@gmail.com>
2017-10-13 09:25:01 -0400

    Initial random sort complete

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <img src="https://www.hp-lexicon.org/wp-content/uploads/2016/09/the_sorting_hat_by_sahinduezguen-d47mwt5.png" />
+   <house></house>
+
+  </div>
+</template>
+
+<script>
+
+import house from './components/houses.vue';
+
+export default {
+  name: 'app',
+  components: { house},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/houses.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="house">
+		<h1>Welcome to {{Houses[this.randomHouse].name}}!!!</h1>
+		<p>{{Houses[this.randomHouse].description}}</p>
+	</div>
+</template>
+
+
+<script>
+	export default {
+	name: 'house',
+	data: function() {
+		return {
+			'Houses': [
+
+		    {
+		      name: 'Gryffindor',
+		      description: 'Here dwell the brave of heart. Your courage and chilvalry are second to none, and you are always up for a good adventure.'
+		    },
+		    {
+		      name: 'Hufflepuff',
+		      description: 'Your honor knows no bounds. Only the honest, fair, and kind need apply.'
+		    },
+		    {
+		      name: 'Ravenclaw',
+		      description: 'Congratulations on being the smartest person in the room! You possess both intelligence and wisdom, as well as a creative spark that sets you apart from your peers.'
+		    },
+		    {
+		      name: 'Slytherin',
+		      description: 'Neither amibition nor cunning are in short supply. You are a survivor, doing whatever it takes to flourish. Prone to pretension.'
+		    }
+
+		  ],
+
+		  randomHouse: Math.floor(Math.random() * 4)
+		}
+	}
+	}
+</script>
